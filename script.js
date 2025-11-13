35486e95d5e6b8ded34d1d180c8d502b1813b6ea
muhammadsayed123 <muhammadsaye741@gmail.com>
2025-11-13 23:24:04 +0600

    adding history section

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const historyData=[]
+
 ///////// Reusable functtion
 
 
 //heart section functionality
+const heart=1;
 function heartFunction(e){
     const mainHeartNumber=parseInt(document.getElementById("heart-num").innerText)
     afterTap=heart+mainHeartNumber
@@ -17,7 +20,7 @@ function heartFunction(e){
 
 
 //heart section functionality
-const heart=1;
+
 
 document.getElementById("heart-btn-1").addEventListener("click",function(){
     heartFunction("heart-btn-1")
@@ -49,6 +52,25 @@ document.getElementById("heart-btn-10").addEventListener("click",function(){
 
 //coin section functionality
 
+function renderHistory(){
+
+    const historyContainer=document.getElementById("history-container")
+    historyContainer.innerHTML = '';
+
+    for(const data of historyData){
+        const div= document.createElement("div")
+        div.innerHTML=`
+        <div class=" bg-gray-100 w-[300px] h-[100px] rounded-lg p-4 flex justify-between">
+                <div>
+                    <h1 class="font-bold">${data.name}</h1>
+                    <p class="text-gray-500">${data.number}</p>
+                </div>
+                <p class="text-gray-500 flex items-center">${data.date}</p>
+        </div><br>
+        `
+        historyContainer.prepend(div)
+    }
+}
 
 document.getElementById("call-btn-1").addEventListener("click",function(){
     const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
@@ -63,25 +85,212 @@ document.getElementById("call-btn-1").addEventListener("click",function(){
     }
     document.getElementById("coin-num").innerText=afterTapCall
     alert(alertMsg)
+    const data={
+        name:"National Emergency Number",
+        number:999,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
     return afterTapCall;
 
+
 })
+
 document.getElementById("call-btn-2").addEventListener("click",function(){
     const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-2-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-2-num").innerText)
     const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
 
-
-
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
     document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Police Helpline Number",
+        number:999,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
     return afterTapCall;
+
 })
+
 document.getElementById("call-btn-3").addEventListener("click",function(){
     const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-3-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-3-num").innerText)
     const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
 
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
     document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Fire Service Number",
+        number:999,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
     return afterTapCall;
+
 })
+
+document.getElementById("call-btn-5").addEventListener("click",function(){
+    const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-5-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-5-num").innerText)
+    const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
+
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
+    document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Ambulance Service",
+        number:1994-999999,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
+    return afterTapCall;
+
+})
+
+document.getElementById("call-btn-6").addEventListener("click",function(){
+    const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-6-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-6-num").innerText)
+    const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
+
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
+    document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Women & Child Helpline",
+        number:109,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
+    return afterTapCall;
+
+})
+
+document.getElementById("call-btn-7").addEventListener("click",function(){
+    const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-7-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-7-num").innerText)
+    const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
+
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
+    document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Anti-Corruption Helpline",
+        number:106,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
+    return afterTapCall;
+
+})
+
+document.getElementById("call-btn-8").addEventListener("click",function(){
+    const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-8-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-8-num").innerText)
+    const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
+
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
+    document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Electricity Helpline",
+        number:16216,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
+    return afterTapCall;
+
+})
+
+document.getElementById("call-btn-9").addEventListener("click",function(){
+    const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-9-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-9-num").innerText)
+    const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
+
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
+    document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Brac Helpline",
+        number:16445,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
+    return afterTapCall;
+
+})
+
+document.getElementById("call-btn-10").addEventListener("click",function(){
+    const mainCoinNumber=parseInt(document.getElementById("coin-num").innerText)
+    const cartText= document.getElementById("cart-10-text").innerText
+    const cartNum= parseInt(document.getElementById("cart-10-num").innerText)
+    const afterTapCall=mainCoinNumber-20
+    alertMsg="Calling "+cartText+" "+cartNum
+
+    if(mainCoinNumber<20){
+        alert("You have not enough coin.")
+        return;
+    }
+    document.getElementById("coin-num").innerText=afterTapCall
+    alert(alertMsg)
+    const data={
+        name:"Bangladesh Railway Helpline",
+        number:163,
+        date:new Date().toLocaleTimeString()
+    }
+    historyData.push(data)
+    renderHistory()
+    return afterTapCall;
+
+})
+
+
+
 
 
 
